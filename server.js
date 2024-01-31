447a31b84ccd83177e83c7ccd5e170159ba25e6e
kutaibta <kutaibaalnizaemy@gmail.com>
2024-01-31 17:23:19 +0300

    implementation webhook

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors());
 app.options("*",cors());
 app.use(compression());
-app.use(express.json());
+app.use(express.json({limit: "20k"}));
 app.use(express.static(path.join(dirname(fileURLToPath(import.meta.url)), '/uploads/')));
 
 if (process.env.NODE_ENV === "development") {
