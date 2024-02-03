cf25815e96f6191c009ff23fb2d816e4cb8a33ba
Kutaiba27 <99476986+Kutaiba27@users.noreply.github.com>
2024-02-03 13:06:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "development") {
    app.use(morgan("dev"));
 }
 
-app.use('/webhook-checkout',express.raw({type: 'application/json'}),webhookCheckout)
+app.post('/webhook-checkout',express.raw({type: 'application/json'}),webhookCheckout)
 
 mountRoutes(app)
 
